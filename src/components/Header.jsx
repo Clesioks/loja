22ab2fcf1b2961aec8f558ade1fccaa504894bab
Clesio <kornalewski@outlook.com>
2024-03-09 19:05:15 -0300

    loja

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
-import logo from '../assets/logo.png'
+import { LinkContainer } from 'react-router-bootstrap'
+import logo from '../assets/logo10.png'
 
 const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                    <Navbar.Brand href="/">
-                        <img src={logo} alt='' />
+                <LinkContainer to="/" >
+                    <Navbar.Brand>
+                        <img src={logo} style={{marginRight:"8px"}} alt='' />
                         Lótus Software
                     </Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id="basic-mavbar-nav">
                         <Nav className='ms-auto'>
-                            <Nav.Link href='/cart'><FaShoppingCart />Carrinho</Nav.Link>
-                            <Nav.Link href='/login'><FaUser />Login</Nav.Link>
+                            <LinkContainer to="/cart">
+                            <Nav.Link><FaShoppingCart />Carrinho</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                            <Nav.Link><FaUser />Login</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
             </Container>
