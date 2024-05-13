3e5e254bbbb523aadf8ce47bed072c1dfe530a92
Clesio <kornalewski@outlook.com>
2024-05-13 08:42:51 -0300

    order screen

--- src/slices/ordersApiSlice.js
+++ src/slices/ordersApiSlice.js
@@ -29,8 +29,19 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
           url: PAYPAL_URL,
         }),
         keepUnusedDataFor: 5,
-      }),
-    })        
+    }),
+    getMyOrders: builder.query({
+        query: () => ({
+            url: `${ORDERS_URL}/mine`
+        }),
+        keepUnusedDataFor: 5,
+    })
+  })   
     })
 
-export const { useGetPaypalClientIdQuery, useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation } = ordersApiSlice
+export const { useGetPaypalClientIdQuery, 
+    useCreateOrderMutation, 
+    useGetOrderDetailsQuery, 
+    usePayOrderMutation,
+    useGetMyOrdersQuery
+ } = ordersApiSlice
